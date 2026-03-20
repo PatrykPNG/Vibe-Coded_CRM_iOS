7a6fbdc20edcf477017a14fa09f58d26d91fc7b7
PatrykPNG <ostrowskiszkola8@gmail.com>
2026-03-20 17:03:16 +0100

    Initial Commit

---
+++ CRM/CRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRMApp.swift
+//  CRM
+//
+//  Created by Patryk Ostrowski on 20/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CRM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CRM
+//
+//  Created by Patryk Ostrowski on 20/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
